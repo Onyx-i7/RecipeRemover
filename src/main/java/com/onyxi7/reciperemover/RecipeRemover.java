ae5c3c7efbd7462e747ecc9e90a6d17f8b8125e0
Onyx_i7 <crackdroid69@gmail.com>
2026-07-06 02:57:12 -0400

    Add the update URL to RecipeRemover and mcmod.info

--- src/main/java/com/onyxi7/reciperemover/RecipeRemover.java
+++ src/main/java/com/onyxi7/reciperemover/RecipeRemover.java
@@ -4,7 +4,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = RecipeRemover.MODID, name = RecipeRemover.NAME, version = RecipeRemover.VERSION, updateJSON = "")
+@Mod(modid = RecipeRemover.MODID, name = RecipeRemover.NAME, version = RecipeRemover.VERSION, updateJSON = "https://raw.githubusercontent.com/Onyx-i7/RecipeRemover/refs/heads/main/modupdatechecker.json")
 public class RecipeRemover {
     public static final String MODID = "reciperemover";
     public static final String NAME = "Recipe Remover";
