ead547e5e1e29850b8ba8385ff3ffe0dfcf024aa
Onyx_i7 <Crackdroid69@gmail.com>
2026-07-07 22:06:14 -0400

    Rename RecipeRemover class to NoRecipeBook

--- src/main/java/com/onyxi7/norecipebook/NoRecipeBook.java
+++ src/main/java/com/onyxi7/norecipebook/NoRecipeBook.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = NoRecipeBook.MODID, name = NoRecipeBook.NAME, version = NoRecipeBook.VERSION, updateJSON = "https://raw.githubusercontent.com/Onyx-i7/NoRecipeBook/refs/heads/main/modupdatechecker.json")
-public class RecipeRemover {
+public class NoRecipeBook {
     public static final String MODID = "norecipebook";
     public static final String NAME = "NoRecipeBook";
     public static final String VERSION = "1.1.0";
