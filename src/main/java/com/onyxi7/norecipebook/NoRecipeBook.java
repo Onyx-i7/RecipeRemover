f06fb8b135cb3544e8c38c5d996928df862540f6
Onyx_i7 <Crackdroid69@gmail.com>
2026-07-07 22:23:13 -0400

    Refactor NoRecipeBook class with proxy and logging

--- src/main/java/com/onyxi7/norecipebook/NoRecipeBook.java
+++ src/main/java/com/onyxi7/norecipebook/NoRecipeBook.java
@@ -1,19 +1,31 @@
 package com.onyxi7.norecipebook;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = NoRecipeBook.MODID, name = NoRecipeBook.NAME, version = NoRecipeBook.VERSION, updateJSON = "https://raw.githubusercontent.com/Onyx-i7/NoRecipeBook/refs/heads/main/modupdatechecker.json")
+@Mod(modid = NoRecipeBook.MODID, name = NoRecipeBook.NAME, version = NoRecipeBook.VERSION)
 public class NoRecipeBook {
     public static final String MODID = "norecipebook";
     public static final String NAME = "NoRecipeBook";
     public static final String VERSION = "1.1.0";
     
     public static Logger logger;
+    
+    @SidedProxy(clientSide = "com.onyxi7.norecipebook.ClientProxy", serverSide = "com.onyxi7.norecipebook.CommonProxy")
+    public static CommonProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        System.out.println("[NoRecipeBook] Loaded correctly");
+        logger = event.getModLog();
+        logger.info("[NoRecipeBook] Pre-initialization complete");
+    }
+    
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init();
+        logger.info("[NoRecipeBook] Initialization complete");
     }
 }
